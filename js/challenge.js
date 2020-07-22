836025148c074fbdd61e5aa548a5310433b3af90
Megan Horsfield <meganthadev@gmail.com>
2020-07-22 05:24:03 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -3,4 +3,5 @@ document.addEventListener("DOMContentLoaded", function() {
  const minus = document.getElementById('minus');
  const plus = document.getElementById('plus');
  const heart = document.getElementById('hear');
- const pause = document.getElementById('pause')
+ const pause = document.getElementById('pause');
+ const 
