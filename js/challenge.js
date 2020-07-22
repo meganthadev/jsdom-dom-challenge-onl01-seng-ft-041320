59b240a23c97b5dac7ff1a3bd2c78e34d6f6faae
Megan Horsfield <meganthadev@gmail.com>
2020-07-22 05:23:51 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -2,5 +2,5 @@ document.addEventListener("DOMContentLoaded", function() {
  const counter = document.getElementById('counter');
  const minus = document.getElementById('-');
  const plus = document.getElementById('+');
- const heart = document.getElementById('<3');
+ const heart = document.getElementById('hear');
  const pause = document.getElementById('pause')
